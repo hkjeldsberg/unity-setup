50be7eca7fe4e00b97f2b7a1f46a979fe48c31a2
hkjeldsberg <henrik.kjeldsberg@live.no>
2018-12-20 00:25:08 +0100

    Simple unity setup.

---
+++ Assets/Gunshot.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Gunshot : MonoBehaviour
+{
+    private float destroyTime;
+
+    void Start()
+    {
+        var sound = this.GetComponent<AudioSource>();
+        destroyTime = sound.clip.length;
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 2000f;
+
+    // Fixed Update for physics updating
+    public void FixedUpdate()
+    {
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0 );
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        } 
+            
+    }
+}
